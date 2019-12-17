c7c4e85807b895faacf1e3a38dc0cd63d660aa1c
admin <admin@Win7_1TB>
2019-12-17 08:55:09 +0100

    Cambiado al ejercicio 3 de la teoría

--- PracticaEntornosRepositorios/src/practicaentornosrepositorios/PracticaEntornosRepositorios.java
+++ PracticaEntornosRepositorios/src/practicaentornosrepositorios/PracticaEntornosRepositorios.java
@@ -19,25 +19,35 @@ public class PracticaEntornosRepositorios {
     public static void main(String[] args) {
         // TODO code application logic here
         double temperaturas[] = new double[7];
-        String diasSemana[] = {"lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"};
-        double suma = 0;
-        int dia;
+        
+        String diaSemana[] = {"lunes", "martes", "miercoles", "jueves", "sabado", "domingo"};
+        double suma = 0, max = 0, min = 0;
+        int dia, posMin = 0, posMax = 0;
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
 
-        System.out.println("Anota temperatura de la semana");
-
-        for (int i = 0; i < temperaturas.length; i++) {
-            System.out.println(diasSemana[i] + ":");
-            temperaturas[i] = sc.nextDouble();
-            suma = suma + temperaturas[i];
-        }
-        System.out.println("La temperatura media ha sido:" + suma / 7);
-        do {
-            System.out.println("Anota día del que quieras saber la temperatura:");
-            dia = sc.nextInt();
-        } while (dia < 1 || dia > 7);
-        System.out.println("La temperatura fue " + temperaturas[dia - 1]);
+        
+            System.out.println("Anota la temperatura de la semana");
+            for (int i = 0; i < temperaturas.length; i++) {
+                System.out.println(diaSemana[i] + ":");
+                temperaturas[i] = sc.nextDouble();
+                suma = suma + temperaturas[i];
+                if (i == 0) {
+                    min = temperaturas[0];
+                    posMin = 0;
+                    max = temperaturas[0];
+                    posMax = 0;
+                } else if (temperaturas[i] >= max) {
+                    max = temperaturas[i];
+                    posMax = i;
+                } else if (temperaturas[i] <= min) {
+                    min = temperaturas[i];
+                    posMin = i;
+                }
+            }
+            System.out.println("La temperatura media ha sido: " + suma / 7);
+            System.out.println("La temperatura máxima fue el día: " + posMax);
+            System.out.println("La temperatura mínima fue el día: " + posMin);
+        } while (respuesta == 's' || respuesta == 'S');
     }
-
 }
